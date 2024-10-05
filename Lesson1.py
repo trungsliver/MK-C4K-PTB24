d3722c6f44c0d45082cff519d334e3084224217d
trungsliver <buiductrung.cva@gmail.com>
2024-10-05 17:19:42 +0700

    Lesson 1

---
+++ Lesson1.py
@@ -0,0 +1,29 @@
+# Hàm print() - hiển thị dữ liệu ra màn hình
+print('Hello world')
+
+# Variables - Biến số
+    # Dùng để lưu trữ dữ liệu
+    # Có thể thay đổi được trong khi lập trình
+teacher = 'Duc Trung'
+
+# Input - Nhập dữ liệu từ bàn phím
+name = input('Hãy nhập tên của bạn: ')
+age = input('Hãy nhập tuổi của bạn: ')
+
+# Các cách hiển thị dữ liệu ra màn hình
+    # Cách 1: Dùng dấu +
+print('Họ tên: ' + name)
+    # Cách 2: Dúng dấu ,
+print('Tuổi:', age)
+    # Cách 3: Dùng f 
+print(f'Tôi tên là {name}, đang {age} tuổi')
+print('Tôi tên là ' + name + ', đang ' + age + ' tuổi')
+print('Tôi tên là', name, ', đang', age, 'tuổi')
+    # Cách 4: Hiển thị trên nhiều dòng
+print('''
+1
+2
+3''')
+
+# Các phép toán cơ bản: + - * /
+print('1 + 1 =',1+1)
