6c58ad781305f59b13effa38fe5fac5e9205c49f
trungsliver <buiductrung.cva@gmail.com>
2024-10-19 14:48:19 +0700

    Lesson 3

---
+++ Lesson3.py
@@ -0,0 +1,24 @@
+# Toán tử số học
+    # Các phép toán thông thường: + - * /
+    # Chia lấy dư: %
+print('20 % 3 =', 20%3)
+    # Chia lấy nguyên: //
+print('20 // 3 =', 20//3)
+    # Lũy thừa: ** 
+print('2**2**3 =', 2**2**3)
+
+# Toán tử số học với string
+    # Phép nối: +
+print('Minh' + ' ' + 'Ngọc')
+    # Phép lặp: *
+print('hi'*3)
+print('Hoàng Nam' * 0)
+
+# Toán tử quan hệ: => True/False
+    # So sánh bằng: ==
+print(5 == 5)       # output: True
+print(5 == 3)       # output: False
+    # So sánh khác: !=
+print(5 != 3)       # output: True
+print(5 != 5)       # output: False
+    # So sánh lớn hơn, nhỏ hơn: >, >=, <, <=
