fbb22cb17ff68a28fd2d41c497f43426f095df33
trungsliver <buiductrung.cva@gmail.com>
2024-10-19 15:19:23 +0700

    Lesson 3

--- Lesson3.py
+++ Lesson3.py
@@ -22,3 +22,20 @@ print(5 == 3)       # output: False
 print(5 != 3)       # output: True
 print(5 != 5)       # output: False
     # So sánh lớn hơn, nhỏ hơn: >, >=, <, <=
+print(5 > 3)        # output: True
+print(4 < 7)        # output: True
+print( 6 >= 6)      # output: True
+print(7 <= 6)       # output: False
+print(8 >= 9)       # output: False
+
+# Toán tử logic: and - or - not
+    # Ví dụ: trà sữa - gà rán
+
+# Biểu thức logic
+x, y, z = 10, 6, 8
+a = x < 12 and z > 6
+b = x > 15 or y < 8
+c = not b
+print(a)       # output: True
+print(b)       # output: True
+print(c)       # output: False
