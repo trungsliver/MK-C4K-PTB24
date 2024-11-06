048a1bf95b20e8cfd5370cd8fb63ab8b6e8e889d
trungsliver <buiductrung.cva@gmail.com>
2024-11-06 10:01:21 +0700

    Lesson 5

---
+++ Lesson5.py
@@ -0,0 +1,12 @@
+# Quy tắc đặt tên file: [Lớp]_[HoTen]_CP1.py
+# Ví dụ: PTB24_DucTrung_CP1.py
+
+# Trắc nghiệm
+# 1A 2B 3C 4D 5E ......
+
+# Tự luận
+# Vấn đáp
+# Tiến Dũng: 1/5
+# Giang Nguyễn: 2/3
+# Hiếu An: 2/3
+# Ngọc Huy: 1.5/5, thực hành 6/10
