989062c079b3ab8541ec0b7ea74c719eb5d626a2
trungsliver <buiductrung.cva@gmail.com>
2024-12-06 13:43:45 +0700

    Lesson 9

---
+++ Lesson9.py
@@ -0,0 +1,13 @@
+# Quy tắc đặt tên file: [Lớp]_[Hoten]_CP2.py
+# Ví dụ: PTB24_DucTrung_CP2.py
+
+# Hạn nộp: 15h15
+
+# github.com/trungsliver/MK-C4K-PTB24
+
+# Tiến Dũng:    1.5/3       10/10       8.5
+# Hiếu An:      2/3         9/10        9
+# Giang Phí:    1.5/3       7/10        7
+# Ngọc Huy:     2.5/3       10/10       9.5
+# Giang Nguyễn: 1.5/3       10/10       9
+# Hoàng Nam:    1/3         7/10        7.5
